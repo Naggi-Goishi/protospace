cb7efab03f3695581da483db80da14aef1b7d2b5
Naggi <naggitapdance@gmail.com>
2016-03-08 20:12:33 +0900

    controller 名 project -> projects

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'top#index'
-  resources :project, only: [:show, :index, :new]
+  resources :projects, only: [:show, :index, :new]
 
 end
