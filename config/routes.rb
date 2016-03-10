421d7229085dbc9445085fb95041210e8eed31df
Naggi <naggitapdance@gmail.com>
2016-03-10 21:38:02 +0900

    Revert "Revert "deleting top controller and making index action in project controller 一緒にコミットするの忘れてました。すみません""
    This reverts commit f68c1941db3c07f0922df36b970c89e479387ab7.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :user
-  root 'top#index'
-  resources :projects, only: [:show, :index, :new]
+  root 'project#index'
+  resources :project, only: [:show, :index, :new]
 
 end
