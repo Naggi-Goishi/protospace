54e5fcf536ec1f80ba9f8fa08c1fa7342d5a2dae
Naggi <naggitapdance@gmail.com>
2016-03-14 01:54:33 +0900

    validation作り

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,14 +10,23 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    current_user.projects.create(create_params)
-    redirect_to projects_path
+    if current_user.projects.create(create_params).valid?
+      redirect_to :root
+    else
+      valid_params
+    end
   end
 
 private
 
   def create_params
     params.require(:project).permit(:title, :catch_copy, :concept, project_images_attributes: [:id, :image, :status])
+  end
+
+  def valid_params
+    @project = Project.new(params.require(:project).permit(:title, :catch_copy, :concept))
+    @project.project_images.build
+    render action: :new
   end
 
 end
--- app/models/project_image.rb
+++ app/models/project_image.rb
@@ -3,4 +3,6 @@ class ProjectImage < ActiveRecord::Base
     belongs_to :project
     mount_uploader :image, ImageUploader
     enum status: [:main, :sub]
+    validates_presence_of :image
+
 end
