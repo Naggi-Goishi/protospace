39e5b27afc9758c3ad325a4bb0ded2b9278dc3f1
Naggi <naggitapdance@gmail.com>
2016-03-19 13:36:03 +0900

    controllerをして指摘通り変えました

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,43 +10,37 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    if current_user.projects.create(create_params).valid?
+    @project = Project.new(create_params)
+    if @project.save
       redirect_to :root
     else
-      valid_action
+      render :new
     end
   end
 
   def destroy
     project = Project.find(params[:id])
-    if project.user_id == current_user.id
-      project.destroy
-    end
     redirect_to :root
   end
 
   def edit
     @project = Project.find(params[:id])
+    unless current_user.id == @project.user_id
+      redirect_to :root
+    else
+    end
   end
 
   def update
     project = Project.find(params[:id])
-    if project.user_id == current_user.id
-      project.update(create_params)
-    end
+    project.save
     redirect_to :root
   end
 
 private
 
   def create_params
-    params.require(:project).permit(:title, :catch_copy, :concept, project_images_attributes: [:id, :image, :status])
-  end
-
-  def valid_action
-    @project = Project.new(params.require(:project).permit(:title, :catch_copy, :concept))
-    @project.project_images.build
-    render action: :new
+    params.require(:project).permit(:title, :catch_copy, :concept, project_images_attributes: [:id, :image, :status]).merge(user_id: current_user.id)
   end
 
 end
