0df62656780653aed43530bc6d1c117328ce2c7b
Naggi <naggitapdance@gmail.com>
2016-03-25 03:51:14 +0900

    likeコントローラー、モデル作成。

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,22 @@
+class LikesController < ApplicationController
+
+before_action :set_likes_project, only: [:create, :destroy]
+
+  def create
+    @like = Like.create(user_id: current_user.id, project_id: params[:project_id])
+  end
+
+  def destroy
+    like = Like.find_by(user_id: current_user.id, project_id: params[:project_id])
+    like.destroy
+  end
+
+private
+
+  def set_likes_project
+      @likes = Like.where(project_id: params[:project_id])
+      @project = Project.find(params[:project_id])
+  end
+
+
+end
