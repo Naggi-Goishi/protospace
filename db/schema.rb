e0bcb3fbdeb40d0b4e18c3c6aefbf7a319db54b6
Naggi <naggitapdance@gmail.com>
2016-03-25 04:08:20 +0900

    commentモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160324162049) do
+ActiveRecord::Schema.define(version: 20160324190441) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",       limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.integer  "project_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
 
   create_table "installs", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
