7817f5d28d0dcd4133e954339272aaa610e92505
Naggi <naggitapdance@gmail.com>
2016-03-25 03:53:04 +0900

    アソシエーション追加

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,13 @@
 class Project < ActiveRecord::Base
 
   has_many :project_images
+  has_many :likes, dependent: :destroy
   belongs_to :user
   accepts_nested_attributes_for :project_images
   validates_presence_of :title, :catch_copy, :concept
+
+  def like_user(user)
+     self.likes.find_by(user_id: user)
+  end
 
 end
