d8611fce0113c75252c435bc306d34eedce754cb
Naggi <naggitapdance@gmail.com>
2016-04-11 22:46:13 +0900

    ネスとしたことによりいらない記述を消した

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,14 +2,13 @@ class CommentsController < ApplicationController
 
   def create
     current_user.comments.create(comment_params)
-    @comments = Comment.where(project_id: params[:comment][:project_id])
-    @comment = Comment.new
+    @comments = Comment.where(project_id: params[:project_id])
   end
 
 private
 
   def comment_params
-    params.require(:comment).permit(:text, :user_id, :project_id)
+    params.require(:comment).permit(:text).merge(project_id: params[:project_id])
   end
 
 end
