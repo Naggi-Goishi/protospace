e4ddadd84c44ffe7ec756eeb847100ed9de250e7
Naggi <naggitapdance@gmail.com>
2016-04-12 19:40:51 +0900

    projectとtagのアソシエーションを書いた

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,6 +3,7 @@ class Project < ActiveRecord::Base
   has_many :project_images
   has_many :likes, dependent: :destroy
   has_many :comments
+  has_many :tags
   belongs_to :user
   accepts_nested_attributes_for :project_images
   validates_presence_of :title, :catch_copy, :concept
