8197a198b65b319266a490186773ff5c58f55d0f
Naggi <naggitapdance@gmail.com>
2016-04-15 14:53:54 +0900

    newestのルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :projects do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: :create
+    collection do
+      get :newest
+    end
   end
   resources :tags, only: [:index, :show], param: 'tag_name'
 end
