1989aeb43a5ed5a004a9cf902be832c779db317d
Naggi <naggitapdance@gmail.com>
2016-04-16 13:38:36 +0900

    newestを孤立

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Rails.application.routes.draw do
   resources :projects do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: :create
-    collection do
-      get :newest
-    end
   end
+  resources :newest, only: :index
   resources :tags, only: [:index, :show], param: 'tag_name'
 end
 
