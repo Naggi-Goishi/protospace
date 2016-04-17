61be122db5bd8daba97250d84717121d3bb7db58
Naggi <naggitapdance@gmail.com>
2016-04-17 19:52:57 +0900

    removing "before_action" and having project_id by params

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,12 @@
 class LikesController < ApplicationController
 
-before_action :set_likes_project, only: [:create, :destroy]
-
   def create
-    @like = current_user.likes.create(project_id: @project.id)
+    @like = current_user.likes.create(project_id: params[:project_id])
     set_likes_project
   end
 
   def destroy
-    like = current_user.likes.find_by(project_id: @project.id)
+    like = current_user.likes.find_by(project_id: params[:project_id])
     like.destroy
     set_likes_project
   end
