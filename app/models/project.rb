522aac10c818e05db24d12f33ac3e5e5a06fd278
Naggi <naggitapdance@gmail.com>
2016-04-23 15:59:24 +0900

    validate only one project_image

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,7 +6,7 @@ class Project < ActiveRecord::Base
   has_many :tags
   belongs_to :user
   accepts_nested_attributes_for :project_images
-  validates_presence_of :title, :catch_copy, :concept
+  validates_presence_of :title, :catch_copy, :concept, :project_images
   acts_as_taggable
 
   def like_user(user)
