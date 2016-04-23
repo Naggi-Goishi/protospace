0a6dc6d40858a8ae6ead40478a1b1e63da1e0889
Naggi <naggitapdance@gmail.com>
2016-04-23 15:58:52 +0900

    fixing error that the images could not be sent

--- app/assets/javascripts/project.js
+++ app/assets/javascripts/project.js
@@ -15,25 +15,6 @@ function show_image(image_id, this_file){
     // 画像がロードされてから処理を行う
     reader.onload = function (e) {
         // readAsDataURLをsrcに指定してイメージを作成
-        $(image_id).append("<img src='"+e.target.result+"'/>");
-    }
-
-    // readAsDataURLメソッドでファイルの内容を取得
-    reader.readAsDataURL(file[0]);
-}
-
-function change_image(image_id, this_file){
-
-    var file = this_file.files;
-
-    // 画像ファイル以外は処理を中止
-    if (!file[0].type.match(/^image\/(png|jpeg|gif)$/)) return;
-
-    var img    = new Image();          // 表示用のイメージオブジェクトを取得
-    var reader = new FileReader();  // ファイルリーダー作成
-    // 画像がロードされてから処理を行う
-    reader.onload = function (e) {
-        // readAsDataURLをsrcに指定してイメージを作成
         console.log("hello")
         $(image_id).html("<img src='"+e.target.result+"'/>");
     }
@@ -42,29 +23,39 @@ function change_image(image_id, this_file){
     reader.readAsDataURL(file[0]);
 }
 
-
 $(document).on("change", "#project_project_images_attributes_0__image", function() {
-    show_image(".cover-image-upload", this)
+    show_image("#image_box0", this)
 });
 
 $(document).on("change", "#project_project_images_attributes_1__image", function() {
-    show_image("#sub_image_0", this)
+    show_image("#image_box1", this)
 });
 
 $(document).on("change", "#project_project_images_attributes_2__image", function() {
-    show_image("#sub_image_1", this)
+    show_image("#image_box2", this)
 });
 
+$(document).on("change", "#project_project_images_attributes_3__image", function() {
+    show_image("#image_box3", this)
+});
+
+
 $(document).on("change", "#project_project_images_attributes_0_image", function() {
-    change_image(".cover-image-upload", this)
+    show_image("#image_box0", this)
 });
 
 $(document).on("change", "#project_project_images_attributes_1_image", function() {
-    show_image("#sub_image_0", this)
+    show_image("#image_box1", this)
 });
 
 
 $(document).on("change", "#project_project_images_attributes_2_image", function() {
-    show_image("#sub_image_1", this)
+    show_image("#image_box2", this)
+});
+
+$('#new_image').on('click',function(){
+    console.log("hello");
+    $(".image-upload-plus").addClass("hide_form");
+    $(".list-group-item.col-md-4.hide_form").removeClass("hide_form");
 });
 
